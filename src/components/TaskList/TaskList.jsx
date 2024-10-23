ac05ab6d8e2f7839132a42faac5c60e6756629cf
jdtoromuriel <jdtoromu@cesde.net>
2024-10-22 19:13:01 -0500

    Create TaskList.jsx

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import TaskItem from './../TaskItem/TaskItem';
+const TaskList = ({ tasks, toggleComplete, removeTask }) => {
+  return (
+    <div className="task-list">
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          toggleComplete={toggleComplete}
+          removeTask={removeTask}
+        />
+      ))}
+    </div>
+  );
+};
+export default TaskList;
